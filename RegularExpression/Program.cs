34e23d2a959236617246c7a9aeed81646d4e0397
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-05 17:44:04 +0530

    [vijayalakshmi] User Registration using regularExpression

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using RegularExpression;
+using System;
+namespace RegEx
+
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            RegExpression regex = new RegExpression();
+
+            Console.WriteLine("Enter Name to check:");
+            Console.Write("First Name:");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.Write("EMAIL:");
+            string email = Console.ReadLine();
+            Console.Write("CONTACT:");
+            string mobileNumber = Console.ReadLine();
+            Console.Write("PASSWORD:");
+            string password = Console.ReadLine();
+            regex.RegexFirstNameChecker(firstName);
+            regex.RegexLastNameChecker(lastName);
+            regex.RegexEmail(email);
+            regex.RegexMobileNumber(mobileNumber);
+            regex.RegexPassword(password);
+            Console.WriteLine("\nSample emails validation:\n");
+            string[] sample = File.ReadAllLines(@"C:\BridgeLabz\.Net_Fellowship\REGEX\RegularExpression\SampleMails.txt");
+            for (int i = 0; i < sample.Length; i++)
+            {
+                string ret = regex.RegexSampleMail(sample[i]);
+                Console.WriteLine(sample[i] + "  --  " + ret);
+            }
+        }
+    }
+}
+    
---
+++ RegularExpression/RegExpression.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class RegExpression
+    {
+        const string FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
+        const string LASTNAME = "^[A-Z]{1}[a-z]{2,}$";
+        const string EMAIL = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$";
+        const string CONTACT = "^[0-9]{1,3}[ ][789][0-9]{9}$";
+        const string PASSWORD = "^[a-z A-Z 0-9]{1,}[A-Z]{1,}[a-z A-Z 0-9]{1,}[!*@#$%^&+=]?[a-z A-Z 0-9]{1,}[0-9]{1,}[a-z A-Z 0-9]{1,}$";
+        const string SAMPLE = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        public void RegexFirstNameChecker(string firstName)
+        {
+            if (Regex.IsMatch(firstName, FIRSTNAME))
+            {
+                Console.WriteLine("First name is valid.");
+                return;
+            }
+            Console.WriteLine("First name is not valid.");
+        }
+        public void RegexLastNameChecker(string secondName)
+        {
+            if (Regex.IsMatch(secondName, LASTNAME))
+            {
+                Console.WriteLine("second name is valid.");
+                return;
+            }
+            Console.WriteLine("second name is not valid.");
+        }
+        public void RegexEmail(string email)
+        {
+            if (Regex.IsMatch(email, EMAIL))
+            {
+                Console.WriteLine("E-Mail id is valid.");
+                return;
+            }
+            Console.WriteLine("E-Mail id is not valid.");
+        }
+        public void RegexMobileNumber(string mobile)
+        {
+            if (Regex.IsMatch(mobile, CONTACT))
+            {
+                Console.WriteLine("Mobile number is valid.");
+                return;
+            }
+            Console.WriteLine("Mobile number is not valid.");
+        }
+        public void RegexPassword(string password)
+        {
+            if (Regex.IsMatch(password, PASSWORD))
+            {
+                Console.WriteLine("Password is valid.");
+                return;
+            }
+            Console.WriteLine("Password is not valid.");
+        }
+        public string RegexSampleMail(string sample)
+        {
+            if (Regex.IsMatch(sample, SAMPLE))
+            {
+                return "Valid";
+            }
+            return "Not Valid";
+        }
+    }
+}
+   
